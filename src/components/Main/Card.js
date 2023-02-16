48d708db8fd158579f7b8c79b5e35163a4f3b6fc
erol23 <hurkanrol15@gmail.com>
2023-02-16 20:01:18 +0100

    data pulled

---
+++ src/components/Main/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = (data) => {
+  return (
+    <div className='cards'>
+        <h1>{data.title}</h1>
+    </div>
+  )
+}
+
+export default Card
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import "./Main.scss";
+import { data } from "../../helpers/data";
+import Card from "./Card";
 
 const Main = () => {
+  console.log(data);
   return (
-    <div>Main</div>
-  )
-}
+    <div className="card-container">
+      {data.map((item) => <Card {...item}/>)}
+    </div>
+  );
+};
 
-export default Main
+export default Main;
